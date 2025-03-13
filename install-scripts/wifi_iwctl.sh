d0389cae93bcda64c39d3eddc0b5e6137544677f
Adrián Tejada <125254174+AdriUMA@users.noreply.github.com>
2025-03-13 12:36:01 +0100

    Update wifi_iwctl.sh

--- install-scripts/wifi_iwctl.sh
+++ install-scripts/wifi_iwctl.sh
@@ -14,14 +14,14 @@ echo
 
 # Reset iwctl (avoid errors)
 echo "${INFO} Reseting iwctl...${RESET}"
-iwctl station "$wifi_device" disconnect > /dev/null 2>&1
+iwctl station "$wifi_device" disconnect #> /dev/null 2>&1
 sleep 0.25
-iwctl device "$wifi_device" set-property Powered off > /dev/null 2>&1
+iwctl device "$wifi_device" set-property Powered off #> /dev/null 2>&1
 sleep 0.25
-iwctl device "$wifi_device" set-property Powered on > /dev/null 2>&1
+iwctl device "$wifi_device" set-property Powered on #> /dev/null 2>&1
 sleep 0.5
-iwctl known-networks "\"$wifi_ssid\"" forget > /dev/null 2>&1
-sleep 0.1
+iwctl known-networks "\"$wifi_ssid\"" forget #> /dev/null 2>&1
+sleep 1
 
 # Connect
 echo "${INFO} Connecting to WiFi network...${RESET}"
