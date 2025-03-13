aa4f6583cbe0ee9a021d247329b2dbc52162c226
Adrián Tejada <125254174+AdriUMA@users.noreply.github.com>
2025-03-13 12:43:24 +0100

    Update check_internet.sh

--- install-scripts/check_internet.sh
+++ install-scripts/check_internet.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 LOG_FILE="script_output.log"
+MAX_ATTEMPTS=15
+DELAY=1
 
 echo "${INFO} Checking internet connection..." | tee -a "$LOG_FILE"
 
-# Check if there is an active internet connection
-if ! ping -c 1 -W 3 archlinux.org &> /dev/null; then
-    echo "${ERROR} No internet connection detected. Aborting..." | tee -a "$LOG_FILE"
-    exit 1
-fi
+attempt=1
+while ! ping -c 1 -W 3 archlinux.org &> /dev/null; do
+    echo "${ERROR} No internet connection detected. Attempt $attempt/$MAX_ATTEMPTS..." | tee -a "$LOG_FILE"
+
+    if [ "$attempt" -ge "$MAX_ATTEMPTS" ]; then
+        echo "${ERROR} No internet after $MAX_ATTEMPTS attempts. Aborting..." | tee -a "$LOG_FILE"
+        exit 1
+    fi
+
+    attempt=$((attempt + 1))
+    sleep "$DELAY"
+done
 
 echo "${OK} Internet connection is active." | tee -a "$LOG_FILE"
