10256498d98d5ee821c0d2ab3d3ceadc07c65bc2
Adrián Tejada <125254174+AdriUMA@users.noreply.github.com>
2025-03-13 11:53:21 +0100

    Update wifi_iwctl.sh

--- install-scripts/wifi_iwctl.sh
+++ install-scripts/wifi_iwctl.sh
@@ -6,13 +6,18 @@ iwctl device list
 echo
 
 custom_read " Please enter the name of your WiFi device${RESET}" wifi_device
-custom_read " Please enter the name of your WiFi network${RESET}" wifi_ssid
+custom_read " Please enter the name (SSID) of your WiFi network${RESET}" wifi_ssid
 custom_read " Please enter the password for your WiFi network${RESET}" wifi_password
 
 echo
 echo "${INFO} Connecting to WiFi network...${RESET}"
 echo
 
-iwctl device "\"$wifi_device\"" set-property Powered on
-iwctl station disconnect
+# Reset iwctl (avoid errors)
+iwctl station disconnect 2>&1 /dev/null
+iwctl device "\"$wifi_device\"" set-property Powered on 2>&1 /dev/null
+iwctl station disconnect 2>&1 /dev/null
+iwctl known-networks "\"$wifi_ssid\"" forget
+
+# Connect
 command "iwctl station $wifi_device connect-hidden \"$wifi_ssid\" --passphrase \"$wifi_password\""
